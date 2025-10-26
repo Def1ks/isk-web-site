4810113f259a2de701851a56c7a0919b3b4368bf
Dinar <dinaraznaev370@gmail.com>
2025-10-26 22:40:38 +0500

    section-1 ready

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ menuToggle.addEventListener('click', function() {
   this.classList.toggle('active');
   menu.classList.toggle('active'); 
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const textElement = document.querySelector('.section-1 .text');
+  textElement.classList.add('visible');
+});
